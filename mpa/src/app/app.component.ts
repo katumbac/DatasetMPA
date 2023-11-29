5c04a8ad51d5f8bcbe7d530074ce5c44b7e6f430
roberto encalada <rocaenca@espol.edu.ec>
2023-11-29 09:59:46 -0500

    Instrucciones de guía 14

--- mpa/src/app/app.component.ts
+++ mpa/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { NavbarComponent } from './shared/navbar/navbar.component'
+import { FooterComponent } from './shared/footer/footer.component'
+import { IndexComponent } from './pages/index/index.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,NavbarComponent,FooterComponent,IndexComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
