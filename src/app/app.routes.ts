66d510c4b3962dc9059aa7fc6a2943c6fe500b3c
roberto encalada <rocaenca@espol.edu.ec>
2023-12-11 09:43:15 -0500

    principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { IndexComponent } from './pages/index/index.component';
 import { AboutComponent } from './pages/about/about.component';
 export const routes: Routes = [
-    { path: '', component: IndexComponent },
+    { path: 'index', component: IndexComponent },
     { path: 'about', component: AboutComponent },
 ];
